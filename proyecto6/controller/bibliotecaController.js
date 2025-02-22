1e102fdeafa66280afb745eb7617c2f8604107cf
Camilo <ch385593@gmail.com>
2025-02-22 01:36:59 -0500

    todos los cambios del proyecto 6, controller y router

--- proyecto6/controller/bibliotecaController.js
+++ proyecto6/controller/bibliotecaController.js
@@ -2,17 +2,45 @@ let db=require("../config/database.js");
  
 class BibliotecaController{
 
-static async registrarLibro(req,res){
-    let {titulo,autor,anio_publicacion,disponibilidad}=req.body;
-    let libro= await db.query("Insert Into libro(titulo, autor, anio_publicacion, disponibilidad) VALUES (?,?,?,?)",[titulo,autor,anio_publicacion,disponibilidad]);
-    res.json(libro);
+//metodos libro
+
+static async registrarLibro(req,res){ //Registro de libro
+    let {titulo,autor,anio_publicacion,disponibilidad}=req.body; //datos para registrar libro en el mysql
+    let libro= await db.query("Insert Into libro(titulo, autor, anio_publicacion, disponibilidad) VALUES (?,?,?,?)",[titulo,autor,anio_publicacion,disponibilidad]); //Comando para insertar los datos 
+    res.json(req.body); //Respuesta con los datos ingresados
 }
+
 static async actualizarLibro(req,res){
     let {id}=req.params;
     let {titulo,autor,anio_publicacion,disponibilidad}=req.body;
-    await db.query("UPDATE libro set titulo=?, autor=?, anio_publicacion=?, disponibilidad=? where id=?"[titulo,autor,anio_publicacion,disponibilidad,id]);
+    await db.query("UPDATE libro SET titulo=?, autor=?, anio_publicacion=?, disponibilidad=? where id=?",[titulo,autor,anio_publicacion,disponibilidad,id]);
     res.json(req.body);
 }
+static async mostrarLibros(req,res){
+    let [lista]=await db.query("SELECT * FROM libro");
+    res.json(lista);
+}
+static async eliminarLibro(req,res){
+    let {id}=req.params;
+    await db.query("DELETE FROM libro WHERE id=?",[id]);
+    res.send({message:"datos eliminados"});
+}
 
+//metodos prestamo
+static async llenarPrestamo(req,res){
+    let {fechaPrestamo,prestamoTotales,idlibro,devolucion,devuelto}=req.body;
+    let prestamo= await db.query("INSERT INTO prestamos(fechaPrestamo, prestamoTotales, idlibro, devolucion, devuelto) VALUES (?,?,?,?,?)",[fechaPrestamo,prestamoTotales,idlibro,devolucion,devuelto]);
+    res.json(req.body);
+}
+static async verPrestamos(req,res){
+    let [libro]=await db.query("SELECT * FROM prestamos");
+    res.json(libro);
+}
+static async modificarPrestamo(req,res){
+    let {id}=req.params;
+    let {fechaPrestamo,prestamoTotales,idlibro,devolucion,devuelto}=req.body;
+    await db.query("UPDATE prestamos SET fechaPrestamo=?, prestamoTotales=?, idlibro=?, devolucion=?, devuelto=? where id=?",[fechaPrestamo,prestamoTotales,idlibro,devolucion,devuelto,id]);
+    res.json(req.body);
+}
 }
 module.exports=BibliotecaController;
--- proyecto6/router/bibliotecaRouter.js
+++ proyecto6/router/bibliotecaRouter.js
@@ -4,5 +4,9 @@ const BibliotecaController=require("../controller/bibliotecaController.js");
 
 router.post("/biblioteca",BibliotecaController.registrarLibro);
 router.put("/biblioteca/:id",BibliotecaController.actualizarLibro);
-
+router.get("/biblioteca",BibliotecaController.mostrarLibros);
+router.delete("/biblioteca/:id",BibliotecaController.eliminarLibro);
+router.post("/prestamo",BibliotecaController.llenarPrestamo)
+router.get("/prestamo",BibliotecaController.verPrestamos);
+router.put("/prestamo/:id",BibliotecaController.modificarPrestamo);
 module.exports = router;
